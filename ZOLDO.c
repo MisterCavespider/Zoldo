d2b1e564e38d309c3fa75a210e645c04d4e543cd
MisterCavespider <cavespider12@gmail.com>
2018-01-15 17:43:49 +0100

    Moved main file

--- ZOLDO.c
+++ ZOLDO.c
@@ -1,38 +1,38 @@
 #include "fxlib.h"
 #include "revolution.h"
-#include "src/game_lib.h"
+// #include "src/game_lib.h"
 #include <stdio.h>
 
 //The address at which the additional RAM of the 9860gii is.
 //Not used by other official apps.
 #define GAME_RAM 0x88040000
 
-int AddIn_main(int isAppli, unsigned short OptionNum)
+int main(int isAppli, unsigned short OptionNum)
 {
-	sprite_t *wall;
-	sprite_t *wall2;
-	sprite_t *player;
-	sprite_t *enemy;
-	sprite_t *ghost;
-
-	entity_t *entity1;
-	entity_t *entity2;
-
-	int ppx = 8;
-	int ppy = 8;
-
-	int kc1 = 0;
-	int kc2 = 0;
-	short unused;
-	unsigned char running = 1;
-
-	char buffer[21];
-
-	Bdisp_AllClr_DDVRAM();
-
-	Bdisp_SetPoint_DDVRAM(0,0,1);
-
-	Bdisp_SetPoint_DDVRAM(2,0,1);
+	// sprite_t *wall;
+	// sprite_t *wall2;
+	// sprite_t *player;
+	// sprite_t *enemy;
+	// sprite_t *ghost;
+    //
+	// entity_t *entity1;
+	// entity_t *entity2;
+    //
+	// int ppx = 8;
+	// int ppy = 8;
+    //
+	// int kc1 = 0;
+	// int kc2 = 0;
+	// short unused;
+	// unsigned char running = 1;
+    //
+	// char buffer[21];
+    //
+	// Bdisp_AllClr_DDVRAM();
+    //
+	// Bdisp_SetPoint_DDVRAM(0,0,1);
+    //
+	// Bdisp_SetPoint_DDVRAM(2,0,1);
 
 	// wall = (sprite_t *)game_cmalloc(sizeof(sprite_t), MEM_EMPTY);
 	// wall->data[0] = 0xBB;
@@ -136,30 +136,30 @@ int AddIn_main(int isAppli, unsigned short OptionNum)
 	// ABOVE THIS POINT ARE ALL MANUAL WRITES
 	// BELOW THIS POINT ARE DYNAMIC LOADS
 
-	game_initialize();
-
-
-	// these are an exception to the rule above, as the memory must first have been allocated
-	g_player->character.x = 0x2000;
-	g_player->character.y = 0x2000;
-	g_player->character.speed = 0x0200;
-	g_player->character.sprite_i = 0;
-	g_player->map_x = 0;
-	g_player->map_y = 0;
-
-	entity1->speed = 256;
-	entity1->x = 0x3000;
-	entity1->y = 0x2000;
-	entity1->sprite_i = 1;
-	entity1->ai_flags = AI_MOVE_FLEE | 0x0500;
-	entity_add(entity1);
-
-	entity2->speed = 256;
-	entity2->x = 0x3000;
-	entity2->y = 0x2000;
-	entity2->sprite_i = 2;
-	entity2->ai_flags = AI_MOVE_PURSUE | 0x0500;
-	entity_add(entity2);
+	// game_initialize();
+    //
+    //
+	// // these are an exception to the rule above, as the memory must first have been allocated
+	// g_player->character.x = 0x2000;
+	// g_player->character.y = 0x2000;
+	// g_player->character.speed = 0x0200;
+	// g_player->character.sprite_i = 0;
+	// g_player->map_x = 0;
+	// g_player->map_y = 0;
+    //
+	// entity1->speed = 256;
+	// entity1->x = 0x3000;
+	// entity1->y = 0x2000;
+	// entity1->sprite_i = 1;
+	// entity1->ai_flags = AI_MOVE_FLEE | 0x0500;
+	// entity_add(entity1);
+    //
+	// entity2->speed = 256;
+	// entity2->x = 0x3000;
+	// entity2->y = 0x2000;
+	// entity2->sprite_i = 2;
+	// entity2->ai_flags = AI_MOVE_PURSUE | 0x0500;
+	// entity_add(entity2);
 
 
 
@@ -168,40 +168,7 @@ int AddIn_main(int isAppli, unsigned short OptionNum)
 	//temp direct load
 	// load_mapchunk(8,8, 0,0, &(g_mapcache->cache[0]));
 
-	game_run();
+	// game_run();
 
 	return 1;
 }
-
-
-//****************************************************************************
-//**************                                              ****************
-//**************                 Notice!                      ****************
-//**************                                              ****************
-//**************  Please do not change the following source.  ****************
-//**************                                              ****************
-//****************************************************************************
-
-
-#pragma section _BR_Size
-unsigned long BR_Size;
-#pragma section
-
-
-#pragma section _TOP
-
-//****************************************************************************
-//  InitializeSystem
-//
-//  param   :   isAppli   : 1 = Application / 0 = eActivity
-//              OptionNum : Option Number (only eActivity)
-//
-//  retval  :   1 = No error / 0 = Error
-//
-//****************************************************************************
-int InitializeSystem(int isAppli, unsigned short OptionNum)
-{
-    return INIT_ADDIN_APPLICATION(isAppli, OptionNum);
-}
-
-#pragma section
---
+++ src/game_main.c
@@ -0,0 +1,39 @@
+#include "src/game_lib.h"	// Will include the rest
+
+// The program starts here
+int AddIn_main(int isAppli, unsigned short OptionNum)
+{
+
+}
+
+//****************************************************************************
+//**************                                              ****************
+//**************                 Notice!                      ****************
+//**************                                              ****************
+//**************  Please do not change the following source.  ****************
+//**************                                              ****************
+//****************************************************************************
+
+
+#pragma section _BR_Size
+unsigned long BR_Size;
+#pragma section
+
+
+#pragma section _TOP
+
+//****************************************************************************
+//  InitializeSystem
+//
+//  param   :   isAppli   : 1 = Application / 0 = eActivity
+//              OptionNum : Option Number (only eActivity)
+//
+//  retval  :   1 = No error / 0 = Error
+//
+//****************************************************************************
+int InitializeSystem(int isAppli, unsigned short OptionNum)
+{
+    return INIT_ADDIN_APPLICATION(isAppli, OptionNum);
+}
+
+#pragma section
