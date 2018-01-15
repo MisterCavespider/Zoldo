674332722bcbf410e0459ca56bbab83fc05f0a2b
MisterCavespider <cavespider12@gmail.com>
2018-01-15 18:03:06 +0100

    Started a stub for multiple file handling

--- src/game_main.c
+++ src/game_main.c
@@ -1,9 +1,38 @@
+#include "fxlib.h"
+#include "revolution.h"
+#include <stdio.h>
 #include "src/game_lib.h"	// Will include the rest
+
+#define STATE_MENU	0
+#define STATE_GAME	1
+
+unsigned char state = 0;
+
+void menu()
+{
+	// Find some files named *.zoldo in storage
+	FONTCHARACTER *pathname = {'\\','\\','f','l','s','0','\\','*','.','z','o','l','d','o', 0};
+	int findHandle;
+	FONTCHARACTER *foundfile = FONTCHARACTER[30];	// 15 more than pathname
+	FILE_INFO fileinfo;
+
+	int err = 0;
+
+	err = Bfile_FindFirst(pathname, &findHandle, foundfile, &fileinfo);
+
+	if(err == 0 && fileinfo.type == DT_FILE)	// Is it usable?
+	{
+
+	}
+}
 
 // The program starts here
 int AddIn_main(int isAppli, unsigned short OptionNum)
 {
-
+	if(state == 0)
+	{
+		menu();
+	}
 }
 
 //****************************************************************************
